fd2e8de4b764e3ee55db46e634078d399bdc06ec
Yasin Mahdizadeh <yasin.mahdizade@gmail.com>
2018-08-14 13:14:44 +0430

    Add files via upload

---
+++ 4-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void f(int x)
+{
+	int i = 5;
+	x = i+1;
+}
+
+int main()
+{
+	int a = 10;
+	f(a);
+	cout << a << endl;
+}
---
+++ 4-2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void f(int x , int& y)
+{
+	int i = 5;
+	x = i+1;
+	y = 18;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 11;
+	f(a,b);
+	cout << a << " " << b << endl;
+}
